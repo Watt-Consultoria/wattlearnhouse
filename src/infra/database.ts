b9f3d12912680a6438d6a1aecf09172f0e0daa95
yanlima <yangclima@gmail.com>
2026-07-10 01:15:24 -0300

    feat: add `prisma` scripts on `package.json`

--- src/infra/database.ts
+++ src/infra/database.ts
@@ -1,7 +1,16 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/generated/prisma/client";
+import { Pool } from "pg";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+});
+
+const adapter = new PrismaPg(pool);
 
 const prisma = new PrismaClient({
   adapter,
